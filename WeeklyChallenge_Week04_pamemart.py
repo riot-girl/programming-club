f6a6a22140ed9fc2c9b61c84abcd30e1d5b3377e
Pamela Martinez Barajas <pamemart@cisco.com>
2021-02-27 10:06:24 -0600

    Headers updated

--- WeeklyChallenge_Week04_pamemart.py
+++ WeeklyChallenge_Week04_pamemart.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2021  Pam Martínez <pamemart@cisco.com>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
-
 """ 
 Level 1. Weekly Challenge 4: For this week’s challenge, you are planning a trip
 overseas.
@@ -38,7 +14,6 @@ __credits__ = ["MXC Programming Club, Luis Uribe"]
 __date__ = "2021/02/26"
 __deprecated__ = False
 __email__ =  "pamemart@cisco.com"
-__license__ = "GPLv3"
 __maintainer__ = "Pam Martínez"
 __status__ = "Development"
 __version__ = "0.0.1"
